16206f205ab9feb1e8015fc99f4cc5bdfcd01306
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-07-15 21:34:32 +0300

    08-context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
-import Order from "./Order";
+import Order from "./Order.jsx";
+import Header from "./Header.jsx";
+import { CartContext } from "./contexts.jsx";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-      <PizzaOfTheDay />
-    </div>
+    <CartContext.Provider value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </CartContext.Provider>
   );
 };
 
