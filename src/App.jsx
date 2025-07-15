84fe9d3c3c5985d18cd9c81d5984e6ead100caa8
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-07-15 21:01:50 +0300

    07-handling-forms

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Order from "./Order";
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's Pizza – Order Now</h1>
+      <h1 className="logo">Padre Gino's Pizza</h1>
       <Order />
       <PizzaOfTheDay />
     </div>
